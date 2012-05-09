018d7d5586293ced9233b85c8762b7338d35c5d6
Jo Greybill <jogreybill@gmail.com>
2012-05-09 10:03:09 -0400

    Added new widgets to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,35 @@
-			<div id="footer">
-				
+			<div class="container" id="footer">
+            
+				<div class="row" id="footer_stories">
+                	<div class="span3">
+                    	(story image goes here)
+                    </div>
+                    <div class="span3">
+                    	(story image goes here)
+                    </div>
+                    <div class="span3">
+                    	(story image goes here)
+                    </div>
+                    <div class="span3">
+                    	(story image goes here)
+                    </div>
+                </div>
+                
+                <div class="row" id="footer_navigation">
+                	<div class="span3">
+                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column One')):?><?php endif;?>
+                    </div>
+                    <div class="span3">
+                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?><?php endif;?>
+                    </div>
+                    <div class="span3">
+                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Three')):?><?php endif;?>
+                    </div>
+                    <div class="span3">
+                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column four')):?><?php endif;?>
+                    </div>
+                </div>
+                
 			</div>
 		</div>
 	</body>
