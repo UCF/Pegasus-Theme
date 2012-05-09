9a318d3b9745008b32f04f15ce745f5a463e136a
Chris Conover <clcono@gmail.com>
2012-05-09 14:49:27 -0400

    Spaces to tabs

--- footer.php
+++ footer.php
@@ -1,41 +1,41 @@
 		</div><!-- close body .container -->
 			<div class="container" id="footer">
-            
+			
 				<div class="row" id="footer_stories">
-                	<div class="span12"><h2>More in Pegasus:</h2></div>
-                	<div class="span3 firststory">
-                    	<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
-                        Story Title Here</a>
-                    </div>
-                    <div class="span3">
-                    	<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
-                        Story Title Here</a>
-                    </div>
-                    <div class="span3">
-                    	<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
-                        Story Title Here</a>
-                    </div>
-                    <div class="span3">
-                    	<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
-                        Story Title Here</a>
-                    </div>
-                </div>
-                
-                <div class="row" id="footer_navigation">
-                	<div class="span3">
-                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column One')):?><?php endif;?>
-                    </div>
-                    <div class="span3">
-                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?><?php endif;?>
-                    </div>
-                    <div class="span3">
-                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Three')):?><?php endif;?>
-                    </div>
-                    <div class="span3">
-                    	<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column four')):?><?php endif;?>
-                    </div>
-                </div>
-                
+					<div class="span12"><h2>More in Pegasus:</h2></div>
+					<div class="span3 firststory">
+						<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
+						Story Title Here</a>
+					</div>
+					<div class="span3">
+						<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
+						Story Title Here</a>
+					</div>
+					<div class="span3">
+						<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
+						Story Title Here</a>
+					</div>
+					<div class="span3">
+						<a href="#"><div class="storyimg" style="background: #333; width: 220px; height: 275px;">Story Image Here</div>
+						Story Title Here</a>
+					</div>
+				</div>
+				
+				<div class="row" id="footer_navigation">
+					<div class="span3">
+						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column One')):?><?php endif;?>
+					</div>
+					<div class="span3">
+						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?><?php endif;?>
+					</div>
+					<div class="span3">
+						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Three')):?><?php endif;?>
+					</div>
+					<div class="span3">
+						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column four')):?><?php endif;?>
+					</div>
+				</div>
+				
 			</div>
 		</div>
 	</body>
