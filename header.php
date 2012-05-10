f33468299a283484d459d2f4db837e4f40974acb
Chris Conover <clcono@gmail.com>
2012-05-10 15:23:45 -0400

    Changed IE detection to JS

--- header.php
+++ header.php
@@ -31,11 +31,7 @@
 		<? } ?>
 		
 	</head>
-	<!--[if lt IE 7 ]>  <body class="ie ie6 <?=body_classes()?>"> <![endif]-->
-	<!--[if IE 7 ]>     <body class="ie ie7 <?=body_classes()?>"> <![endif]-->
-	<!--[if IE 8 ]>     <body class="ie ie8 <?=body_classes()?>"> <![endif]-->
-	<!--[if IE 9 ]>     <body class="ie ie9 <?=body_classes()?>"> <![endif]-->
-	<!--[if (gt IE 9)|!(IE)]><!--> <body class="<?=body_classes()?>"> <!--<![endif]-->
+	 <body class="<?=body_classes()?>">
 		<div id="ipad" class="modal">
 			<div class="modal-header">
 				<h3>Pegasus Magazine is Available on the iPad!</h3>
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,16 @@
 if (typeof jQuery != 'undefined'){
 	jQuery(document).ready(function($) {
+		if($.browser.msie) {
+			var version = $.browser.version;
+			if(version >= 7 && version < 8) {
+				$('body').addClass('ie7');
+			} else if(version >= 8 && version < 9) {
+				$('body').addClass('ie8');
+			} else if(version >= 9 && version < 10) {
+				$('body').addClass('ie9');
+			}
+		}
+
 		Webcom.slideshow($);
 		Webcom.chartbeat($);
 		Webcom.analytics($);
