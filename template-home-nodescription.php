547cb06c05a2c8680faf45a49d0d1086aca68a8a
Jo Greybill <jogreybill@gmail.com>
2012-05-14 15:13:37 -0400

    Added custom structure for home page

--- template-home-nodescription.php
+++ template-home-nodescription.php
@@ -1,16 +1,42 @@
-<?php get_header();?>
+<div class="container wide" id="home_header">
+			<div class="row">
+				<div class="span12">
+					<div class="row" style="position:relative;">
+						<a href="<?=site_url()?>" class="span3 title">
+							PEGASUS
+						</a>
+
+						<div class="span4 offset5 description">
+							The Magazine of the University of Central Florida
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="span5 edition">
+					Summer 2012
+				</div>
+			</div>
+		</div>
+<div class="container">
 	<?php $options = get_option(THEME_OPTIONS_NAME);?>
 	<?php $page    = get_page_by_title('Home');?>
 	<div class="row page-content nodescription" id="home" data-template="home-nodescription">
-		
+
 		<?php $content = str_replace(']]>', ']]&gt;', apply_filters('the_content', $page->post_content));?>
 		<?php if($content):?>
 		<?=$content?>
-		<?php elseif($page == null):?>
-		<p>To edit this content, <a href="<?=get_admin_url()?>post-new.php?post_type=page">create a new page</a> titled "Home" and add your content. The home page image can be set by selecting an uploaded file on the <a href="<?=get_admin_url()?>admin.php?page=theme-options#site">theme options page</a> in the admin.</p>
-		<?php endif;?>
+		<?php endif; ?>
 	
 		
 	</div>
 
 <?php get_footer();?>
+<style type="text/css">
+	body, #footer { 
+		background: #031123;
+	}
+	#footer_navigation {
+		border-top: 1px solid #3f3d3d;
+	}
+</style>
