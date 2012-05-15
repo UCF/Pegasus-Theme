62b34b8216b2d836efc2939b07eaca50ad52b152
Chris Conover <clcono@gmail.com>
2012-05-15 11:31:47 -0400

    Adjusted footer styles

--- footer.php
+++ footer.php
@@ -1,43 +1,45 @@
 		</div><!-- close body .container -->
 		<div id="footer">
 			<div class="container wide">
-
-					<div class="row" id="footer_stories">
-						<div class="span12"><h2>More in this Issue</h2></div>
-						
-						<? 
-						foreach(get_navigation_stories() as $story) {
-						?> 
-							<div class="span3">
-								<a href="<?=get_permalink($story->ID)?>">
-									<div class="thumb">
-										<img src="<?=get_featured_image_url($story->ID)?>" />
-									</div>
-									<div class="title">
-										<?=apply_filters('the_title', $story->post_title)?>
-									</div>
-								</a>
-							</div>
-						<? } ?>
-						
-					</div>
+				<div class="row" id="footer_stories">
+					<div class="span12"><h2>More in this Issue</h2></div>
 					
-					<div class="row" id="footer_navigation">
+					<? 
+					foreach(get_navigation_stories() as $story) {
+					?> 
 						<div class="span3">
-							<a href="<?=site_url()?>"><h2 id="footer_logo">Pegasus</h2></a>
-							<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column One')):?><?php endif;?>
+							<a href="<?=get_permalink($story->ID)?>">
+								<div class="thumb">
+									<img src="<?=get_featured_image_url($story->ID)?>" />
+								</div>
+								<div class="title">
+									<?=apply_filters('the_title', $story->post_title)?>
+								</div>
+							</a>
 						</div>
-						<div class="span3">
-							<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?><?php endif;?>
-						</div>
-						<div class="span3">
-							<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Three')):?><?php endif;?>
-						</div>
-						<div class="span3">
-							<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column four')):?><?php endif;?>
+					<? } ?>
+					
+				</div>
+				
+				<div class="row" id="footer_navigation">
+					<div class="span12">
+						<div class="row">
+							<div class="span3">
+								<a href="<?=site_url()?>"><h2 id="footer_logo">Pegasus</h2></a>
+								<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column One')):?><?php endif;?>
+							</div>
+							<div class="span3">
+								<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?><?php endif;?>
+							</div>
+							<div class="span3">
+								<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Three')):?><?php endif;?>
+							</div>
+							<div class="span3">
+								<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column four')):?><?php endif;?>
+							</div>
 						</div>
 					</div>
-				
+				</div>
 			</div>
 		</div>
 	</body>
