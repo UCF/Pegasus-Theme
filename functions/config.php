bff2f9c80909030eb09c6bb482cc3d41239aafe5
Jo Greybill <jogreybill@gmail.com>
2012-09-19 12:17:53 -0400

    Re-added missing image size

--- functions/config.php
+++ functions/config.php
@@ -12,7 +12,7 @@ function __init__(){
 	add_theme_support('menus');
 	add_theme_support('post-thumbnails');
 	add_image_size('homepage', 620);
-	add_image_size('homepage-secondary', 540);
+	add_image_size('single-post-thumbnail', 220, 230, true);
 	register_nav_menu('header-menu', __('Header Menu'));
 	register_nav_menu('footer-menu', __('Footer Menu'));
 	register_sidebar(array(
