f62eebbee5128446ff117fd846d23f6a2c8aeb5b
Jo Greybill <jogreybill@gmail.com>
2012-10-02 08:34:54 -0400

    Added peach fill logo to home page header, footer

--- dev/2012/fall/home/home.js
+++ dev/2012/fall/home/home.js
@@ -4,5 +4,5 @@ $(function() {
 	$('body').attr('style', 'background-image: url("'+bodyBg+'"); background-repeat: no-repeat; background-position: top center; background-size: cover;');
 	
 	// Set Pegasus logos
-	$('#header .title, h2#footer_logo').addClass('outline');
+	$('#header .title, h2#footer_logo').addClass('peach');
 });
