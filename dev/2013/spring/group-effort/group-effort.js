67bef497f893badf0c6b564004317435e5e346b9
Jo Greybill <jogreybill@gmail.com>
2013-02-25 11:46:05 -0500

    Javascript bug fixes; replaced images with smaller versions; updated some styles

--- dev/2013/spring/group-effort/group-effort.js
+++ dev/2013/spring/group-effort/group-effort.js
@@ -8,42 +8,85 @@ var setImgDimensions = function() {
 	$('.stackable-img').css({
 		'width' : $('.stackable-col').width(),
 		'height' : $(window).height()
+	}).each(function() {
+		// If the .stackable-img col is wider than its child image,
+		// resize the image so that it is as wide/tall as its parent
+		// while maintaining its aspect ratio:
+		var stackableImg = $(this),
+			img = stackableImg.children('img'),
+			oldImgWidth = img.width(),
+			oldImgHeight = img.height(),
+			newImgWidth = stackableImg.width();
+			newImgHeight = stackableImg.height();
+		
+		if (img.width() < stackableImg.width()) {
+			img.width(newImgWidth);
+			img.height( Math.round((newImgWidth * oldImgHeight) / oldImgWidth) ); 
+		}
+		// Default
+		else {
+			img.width(stackableImg.width());
+			img.css('height', 'auto');
+		}
+		
+		// Vertical alignment adjustment
+		if (img.height() < stackableImg.height()) {
+			img.css( 'padding-top', Math.round((stackableImg.height() - img.height()) / 2) );
+		}
+		else { 
+			img.css('padding-top', '');
+		}
+	});
+	
+}
+var setColHeights = function() {
+	// Force .stackable-col's to always be of equal or greater height of their
+	// sibling .text-col:
+	
+	// Reset forced CSS values from any previous screen resize:
+	$('.stackable-col, .text-col').css('height', '');
+	
+	$('.stackable-col').each(function() {
+		var stackableCol 			= $(this),
+			stackableColHeight 		= stackableCol.height(),
+			siblingTextCol 			= stackableCol.next('.text-col'),
+			siblingTextColHeight 	= siblingTextCol.height(),
+			textColContent 			= siblingTextCol.children('.text-col-content'),
+			textColContentHeight 	= textColContent.height();
+		
+		// Determine the actual height of siblingTextCol based
+		// on its contents:
+		if (siblingTextColHeight < textColContentHeight) {
+			siblingTextColHeight = textColContentHeight;
+		}
+		
+		// Set col heights:	
+		if (siblingTextColHeight > stackableColHeight) {
+			stackableCol.height(siblingTextColHeight);
+		}
+		else  {
+			siblingTextCol.height(stackableColHeight);
+		}
 	});
 }
-var setImgColHeight = function() {
-	// Force .stackable-col's to always be the same height as their sibling .text-col:
-	$('.stackable-col').each(function() {
-		var stackableCol = $(this),
-			siblingTextCol = stackableCol.next('.text-col');
-			
-		if ( ($(window).height() < siblingTextCol.height()) || ($(window).height() < stackableCol.height()) ) {
-			if (siblingTextCol.height() > stackableCol.height()) {
-				stackableCol.css('height', siblingTextCol.height());
-			}
-			else {
-				siblingTextCol.height(stackableCol.height());
-			}
-		}
-		else {
-			stackableCol.css('height', $(window).height());
-			siblingTextCol.height($(window).height());
-		}
+var resetColAdjustments = function() {
+	// If the window has been resized to below 768px, reset column
+	// widths/heights and any additional padding to images:
+	$('.stackable-col, .stackable-img, .stackable-img img, .text-col').css({
+		'height' : '',
+		'width' : '',
 	});
-}/*
-var setFixedVerticalPos = function() {
-	// Adjust the negative margin-top value of .fixedPos to always
-	// force it to the vertical center of its .stackable-col
-	// See: http://css-tricks.com/quick-css-trick-how-to-center-an-object-exactly-in-the-center/
-	if ($('.fixedPos').length > 0) {
-		$(this).css('margin-top', ($(this).height() / 2) * -1);
-	}
-}*/
+	$('.stackable-img img').css('padding-top','');
+	$('.fixedPos').removeClass('fixedPos');
+	$('.bottomPos').removeClass('bottomPos');
+}
 
 $(window).load(function() {	
-	var endPos = getEndPos();
-	setImgDimensions();
-	setImgColHeight();
-	//setFixedVerticalPos();
+	if ($(window).width() > 767) {
+		var endPos = getEndPos();
+		setImgDimensions();
+		setColHeights();
+	}
 
 	$(window).scroll(function() {
 		if ($(window).width() > 767) {
@@ -60,23 +103,26 @@ $(window).load(function() {
 					.toggleClass('bottomPos', endPos <= currentPos);
 			});	
 			
-			
+			// If we've reached the end of the main story container,
+			// unset the current stackable fixed image so it sits
+			// at the bottom of its parent column:
 			if ($('.fixedPos').length > 0) {
-				// If we've reached the end of the main story container,
-				// unset the current stackable fixed image:
 				if (endPos <= currentPos) {
 					$('.fixedPos').removeClass('fixedPos').addClass('bottomPos');
 				}
-				//setFixedVerticalPos();
 			}
 		}
 	});
 	
 	$(window).resize(function() {
-		endPos = getEndPos();
-		setImgDimensions();	
-		setImgColHeight();
-		//setFixedVerticalPos();
+		if ($(window).width() > 767) {
+			endPos = getEndPos();
+			setImgDimensions();	
+			setColHeights();
+		}
+		else {
+			resetColAdjustments();
+		}
 	});
 
 });
