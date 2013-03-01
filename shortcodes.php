5816433202a93fd3afec9413a69bac4d8223acfe
Jo Greybill <jogreybill@gmail.com>
2013-03-01 12:10:27 -0500

    Added shortcode for grabbing arbitrary media URL; updated alma mater markup with audio player (WIP)

--- shortcodes.php
+++ shortcodes.php
@@ -115,4 +115,31 @@ function sc_static_image($attr) {
 }
 add_shortcode('static-image', 'sc_static_image');
 
+
+/* 
+ * Search for some arbitrary media in the media library.
+ */
+function sc_get_media($attr) {
+	global $wpdb, $post;
+
+	$post_id = wp_is_post_revision($post->ID);
+	if($post_id === False) {
+		$post_id = $post->ID;
+	}
+
+	$url = '';
+	if(isset($attr['filename']) && $attr['filename'] != '') {
+		$sql = sprintf('SELECT * FROM %s WHERE post_title="%s" AND post_parent=%d ORDER BY post_date DESC', $wpdb->posts, $wpdb->escape($attr['filename']), $post_id);
+		$rows = $wpdb->get_results($sql);
+		if(count($rows) > 0) {
+			$obj = $rows[0];
+			if($obj->post_type == 'attachment') {
+				$url = wp_get_attachment_url($obj->ID);
+			}
+		}
+	}
+	return $url;
+}
+add_shortcode('media', 'sc_get_media');
+
 ?>
