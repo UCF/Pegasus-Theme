21ede9dfd655bc6ba0ee042d8306cc6b09d69ca2
Jo Greybill <jogreybill@gmail.com>
2013-03-06 13:35:36 -0500

    Mighty Mascots markup + styles

---
+++ dev/2013/spring/mighty-mascots/mighty-mascots.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	$('#puff').tooltip({
+		placement: 'top',
+		title: 'Puff'
+	});
+
+	$('#citronaut').tooltip({
+		placement: 'left',
+		title: 'Citronaut'
+	});
+
+	$('#knightro1996').tooltip({
+		placement: 'top',
+		title: '1996 Knightro'
+	});
+
+	$('#glycerin').tooltip({
+		placement: 'bottom',
+		title: 'Glycerin'
+	});
+
+	$('#wins-a-lot').tooltip({
+		placement: 'top',
+		title: 'Sir Wins-a-lot'
+	});
+
+	$('#mack').tooltip({
+		placement: 'bottom',
+		title: 'Mack the Knight'
+	});
+
+	$('#knightro').tooltip({
+		placement: 'right',
+		title: 'Knightro'
+	});
+
+	$('#vulture').tooltip({
+		placement: 'right',
+		title: 'Vulture'
+	});
+
+});
