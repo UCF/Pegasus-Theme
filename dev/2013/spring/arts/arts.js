0fa04610fcddc16f1449cd521745ed07cab4888f
Jo Greybill <jogreybill@gmail.com>
2013-03-06 10:47:54 -0500

    Style adjustments

--- dev/2013/spring/arts/arts.js
+++ dev/2013/spring/arts/arts.js
@@ -1,9 +1,2 @@
-$(window).load(function() {
-	/* Delete annoying empty p tags */
-	$("p:empty").remove();
-	$('#story-img-wrap, #story-title').height($('#story-title-bg').height());
-});
-
-$(window).resize(function() {
-	$('#story-img-wrap, #story-title').height($('#story-title-bg').height());
-});
+/* Delete annoying empty p tags */
+$("p:empty").remove();
