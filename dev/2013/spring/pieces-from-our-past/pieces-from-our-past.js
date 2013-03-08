9307a98d824a6fb0d4a303fb8e072ff5e43918db
Jo Greybill <jogreybill@gmail.com>
2013-03-08 09:52:09 -0500

    Pieces From Our Past updated w/FlexSlider

--- dev/2013/spring/pieces-from-our-past/pieces-from-our-past.js
+++ dev/2013/spring/pieces-from-our-past/pieces-from-our-past.js
@@ -1,54 +1,88 @@
-$(document).ready(function() {
-	var pieces = $('#myCarousel .item');
-	var totalPieces = pieces.size() - 1;
-	var currentIndex = 0;
-
-	$('#myCarousel').carousel({
-		interval: false
-	});
-
-	$('.p-left').click(function() {
-		currentIndex--;
-		updateNavigation();
-		$('#myCarousel').carousel('prev');
-	})
-
-	$('.p-right').click(function() {
-		currentIndex++;
-		updateNavigation();
-		$('#myCarousel').carousel('next');
-	})
-
-	function updateNavigation() {
-		if (currentIndex < 0) {
-			currentIndex = totalPieces;
+var updateMobileSlider = function() {
+	// Change out the img src per each li
+	if ($(window).width() < 768) {
+		$('#slider .slides li').each(function() {
+			var li = $(this),
+				img = li.children('img'),
+				caption = li.children('.flex-caption');
+			img.attr('src', li.attr('data-mobile-img'));
+			// Adjust captions. Right floating captions need special handling
+			if (caption.hasClass('right-float-wrap')) {
+				caption.css('width', '5%');
+			}
+			else {
+				caption.css('width', 'auto');
+			}
+		});
+	}
+	// Set the img's back to their normal src if the window has been upsized
+	else {
+		var firstLi = $('#slider .slides li:first-child');
+		// Just check the first li initially
+		if (firstLi.attr('data-mobile-img') == firstLi.children('img').attr('src')) {
+			$('#slider .slides li').each(function() {
+				var li = $(this),
+					img = li.children('img');					
+				img.attr('src', li.attr('data-normal-img'));
+			});
 		}
 		
-		if (currentIndex > totalPieces) {
-			currentIndex = 0;
-		}
-
-		var prevPieceIndex = currentIndex - 1;
-		if (prevPieceIndex < 0) {
-			prevPieceIndex = totalPieces;
-		}
-
-		var nextPieceIndex = currentIndex + 1;
-		if (nextPieceIndex > totalPieces) {
-			nextPieceIndex = 0;
-		}
-
-		var prevImgSrc = $(pieces.get(prevPieceIndex)).find('img').attr('src');
-		var nextImgSrc = $(pieces.get(nextPieceIndex)).find('img').attr('src');
-		var prevImg = $('#myCarousel .p-left img');
-		var nextImg = $('#myCarousel .p-right img');
-		prevImg.fadeOut(500, function() {
-			$(this).attr('src', prevImgSrc.replace('.png', '-thumb.jpg'));
+		// Update caption widths on every resize above 767px.
+		// Caption widths can't be specified in percents so we have to calculate it:
+		$('#slider .slides li').each(function() {
+			var li = $(this),
+				caption = li.children('.flex-caption');
+			var captionWidth = Math.round( li.css('width').slice(0, -2) * li.attr('data-caption-width') ) / 100;
+			if (caption.hasClass('right-float-wrap')) {
+				caption.children('.right-float').css('width', captionWidth);
+			}
+			else {
+				caption.css('width', captionWidth);
+			}
 		});
-		nextImg.fadeOut(500, function() {
-			$(this).attr('src', nextImgSrc.replace('.png', '-thumb.jpg'));
-		});
-		prevImg.fadeIn(500);
-		nextImg.fadeIn(500);
 	}
+}
+
+$(window).load(function() {
+	
+	// Activate FlexSlider	
+	$.getScript(THEME_JS_URL + '/flexslider/jquery.flexslider-min.js', function() {
+		// The slider being synced must be initialized first
+		$('#slider-nav').flexslider({
+			animation: 'slide',
+			controlNav: false,
+			animationLoop: false,
+			slideshow: false,
+			itemWidth: 150,
+			itemMargin: 30,
+			minItems: 5,
+			maxitems: 10,
+			touch: true,
+			asNavFor: '#slider'
+		});
+		   
+		$('#slider').flexslider({
+			animation: 'slide',
+			controlNav: false,
+			animationLoop: false,
+			slideshow: false,
+			touch: true,
+			keyboard: true,
+			sync: '#slider-nav',
+			useCSS: true,
+		});	
+	});
+	
+	
+	// Slider resizing
+	updateMobileSlider();
+	
+	var timeout = false;
+	$(window).resize(function() {
+		if (timeout !== false) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(updateMobileSlider, 200);
+	});
+	
 });
