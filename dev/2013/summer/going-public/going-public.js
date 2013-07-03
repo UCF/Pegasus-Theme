f433cb8e602bdf38e361f72d00f58dc27e9d578f
Brandon Groves <brandon.groves@gmail.com>
2013-07-03 16:03:27 -0400

    going public initial checkin

--- dev/2013/summer/going-public/going-public.js
+++ dev/2013/summer/going-public/going-public.js
@@ -1,4 +1,175 @@
+(function($) {
+    $.fn.countTo = function(options) {
+        // merge the default plugin settings with the custom options
+        options = $.extend({}, $.fn.countTo.defaults, options || {});
+
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(options.speed / options.refreshInterval),
+            increment = (options.to - options.from) / loops;
+
+        return $(this).each(function() {
+            var _this = this,
+                loopCount = 0,
+                value = options.from,
+                interval = setInterval(updateTimer, options.refreshInterval);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+                $(_this).html(value.toFixed(options.decimals));
+
+                if (typeof(options.onUpdate) == 'function') {
+                    options.onUpdate.call(_this, value);
+                }
+
+                if (loopCount >= loops) {
+                    clearInterval(interval);
+                    value = options.to;
+
+                    if (typeof(options.onComplete) == 'function') {
+                        options.onComplete.call(_this, value);
+                    }
+                }
+            }
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 0,  // the number the element should start at
+        to: 100,  // the number the element should end at
+        speed: 1000,  // how long it should take to count between the target numbers
+        refreshInterval: 100,  // how often the element should be updated
+        decimals: 0,  // the number of decimal places to show
+        onUpdate: null,  // callback method for every time the element is updated,
+        onComplete: null,  // callback method for when the element finishes updating
+    };
+})(jQuery);
+
+/* ==========================================================
+ * bootstrap-affix.js v2.3.2
+ * http://twitter.github.com/bootstrap/javascript.html#affix
+ * ==========================================================
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
+!function ($) {
+
+  "use strict"; // jshint ;_;
+
+
+ /* AFFIX CLASS DEFINITION
+  * ====================== */
+
+  var Affix = function (element, options) {
+    this.options = $.extend({}, $.fn.affix.defaults, options)
+    this.$window = $(window)
+      .on('scroll.affix.data-api', $.proxy(this.checkPosition, this))
+      .on('click.affix.data-api',  $.proxy(function () { setTimeout($.proxy(this.checkPosition, this), 1) }, this))
+    this.$element = $(element)
+    this.checkPosition()
+  }
+
+  Affix.prototype.checkPosition = function () {
+    if (!this.$element.is(':visible')) return
+
+    var scrollHeight = $(document).height()
+      , scrollTop = this.$window.scrollTop()
+      , position = this.$element.offset()
+      , offset = this.options.offset
+      , offsetBottom = offset.bottom
+      , offsetTop = offset.top
+      , reset = 'affix affix-top affix-bottom'
+      , affix
+
+    if (typeof offset != 'object') offsetBottom = offsetTop = offset
+    if (typeof offsetTop == 'function') offsetTop = offset.top()
+    if (typeof offsetBottom == 'function') offsetBottom = offset.bottom()
+
+    affix = this.unpin != null && (scrollTop + this.unpin <= position.top) ?
+      false    : offsetBottom != null && (position.top + this.$element.height() >= scrollHeight - offsetBottom) ?
+      'bottom' : offsetTop != null && scrollTop <= offsetTop ?
+      'top'    : false
+
+    if (this.affixed === affix) return
+
+    this.affixed = affix
+    this.unpin = affix == 'bottom' ? position.top - scrollTop : null
+
+    this.$element.removeClass(reset).addClass('affix' + (affix ? '-' + affix : ''))
+  }
+
+
+ /* AFFIX PLUGIN DEFINITION
+  * ======================= */
+
+  var old = $.fn.affix
+
+  $.fn.affix = function (option) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('affix')
+        , options = typeof option == 'object' && option
+      if (!data) $this.data('affix', (data = new Affix(this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  $.fn.affix.Constructor = Affix
+
+  $.fn.affix.defaults = {
+    offset: 0
+  }
+
+
+ /* AFFIX NO CONFLICT
+  * ================= */
+
+  $.fn.affix.noConflict = function () {
+    $.fn.affix = old
+    return this
+  }
+
+
+ /* AFFIX DATA-API
+  * ============== */
+
+  $(window).on('load', function () {
+    $('[data-spy="affix"]').each(function () {
+      var $spy = $(this)
+        , data = $spy.data()
+
+      data.offset = data.offset || {}
+
+      data.offsetBottom && (data.offset.bottom = data.offsetBottom)
+      data.offsetTop && (data.offset.top = data.offsetTop)
+
+      $spy.affix(data)
+    })
+  })
+
+
+}(window.jQuery);
+
 $(document).ready(function($) {
+    $('#journey-wrapper').affix({
+        offset: {
+            top: 200,
+            bottom: 2000,
+        }
+    });
+
     $.getScript(THEME_JS_URL + '/waypoints.min.js').done(function(script, textStatus) {
 
         // Invention Disclosures Reviewed by OTT
@@ -58,8 +229,6 @@ $(document).ready(function($) {
                 });
             }, {offset: '50%', triggerOnce: 'true'});
 
-            // # of Available for Licensing
-
             // % of Licenses Executed to Spinout
             $("#license-spinout").waypoint(function() {
 
@@ -85,8 +254,6 @@ $(document).ready(function($) {
                 });
             }, {offset: '50%', triggerOnce: 'true'});
 
-            // # of Licenses Executed
-
             // Royalty Distribution
             $("#royalty-distribution").waypoint(function() {
 
@@ -103,54 +270,43 @@ $(document).ready(function($) {
 
                 RGraph.Effects.Pie.RoundRobin(pie, {'radius': false,'frames':60});
 
-            }, {offset: '50%', triggerOnce: 'true'});
+            }, {offset: '70%', triggerOnce: 'true'});
         });
+
+        // pipe light up
+        $("#public-header").waypoint(function() {
+            $('#process-pipe-wrapper .pipe-blue').removeClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-green').removeClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-orange').removeClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-red').removeClass('pipe-off');
+        });
+
+        $("#public-research").waypoint(function() {
+            $('#process-pipe-wrapper .pipe-blue').removeClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-green').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-orange').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-red').addClass('pipe-off');
+        }, {offset: '30%'});
+
+        $("#public-protect").waypoint(function() {
+            $('#process-pipe-wrapper .pipe-blue').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-green').removeClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-orange').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-red').addClass('pipe-off');
+        }, {offset: '40%'});
+
+        $("#public-promote").waypoint(function() {
+            $('#process-pipe-wrapper .pipe-blue').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-green').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-orange').removeClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-red').addClass('pipe-off');
+        }, {offset: '50%'});
+
+        $("#public-commercialize").waypoint(function() {
+            $('#process-pipe-wrapper .pipe-blue').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-green').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-orange').addClass('pipe-off');
+            $('#process-pipe-wrapper .pipe-red').removeClass('pipe-off');
+        }, {offset: '50%'});
     });
 });
-
-(function($) {
-    $.fn.countTo = function(options) {
-        // merge the default plugin settings with the custom options
-        options = $.extend({}, $.fn.countTo.defaults, options || {});
-
-        // how many times to update the value, and how much to increment the value on each update
-        var loops = Math.ceil(options.speed / options.refreshInterval),
-            increment = (options.to - options.from) / loops;
-
-        return $(this).each(function() {
-            var _this = this,
-                loopCount = 0,
-                value = options.from,
-                interval = setInterval(updateTimer, options.refreshInterval);
-
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-                $(_this).html(value.toFixed(options.decimals));
-
-                if (typeof(options.onUpdate) == 'function') {
-                    options.onUpdate.call(_this, value);
-                }
-
-                if (loopCount >= loops) {
-                    clearInterval(interval);
-                    value = options.to;
-
-                    if (typeof(options.onComplete) == 'function') {
-                        options.onComplete.call(_this, value);
-                    }
-                }
-            }
-        });
-    };
-
-    $.fn.countTo.defaults = {
-        from: 0,  // the number the element should start at
-        to: 100,  // the number the element should end at
-        speed: 1000,  // how long it should take to count between the target numbers
-        refreshInterval: 100,  // how often the element should be updated
-        decimals: 0,  // the number of decimal places to show
-        onUpdate: null,  // callback method for every time the element is updated,
-        onComplete: null,  // callback method for when the element finishes updating
-    };
-})(jQuery);
