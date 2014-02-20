74debe052ccd9c189cd78f4db5ecba03e7e962ea
Jo Greybill <jogreybill@gmail.com>
2014-02-20 14:56:46 -0500

    Header image stretch fixes in IE8; set muted color for slideshow first/last arrows

--- templates/story/default.php
+++ templates/story/default.php
@@ -7,7 +7,7 @@
 
 <article class="story story-default">
 	<?php if ($header_img_id) { ?>
-	<div class="container-wide story-header-image" style="background-image: url('<?=$header_img?>'); filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=$header_img?>',sizingMethod='scale')">
+	<div class="container-wide story-header-image" style="background-image: url('<?=$header_img?>');">
 		<img src="<?=$header_img?>" alt="<?=$post->post_title?>" title="<?=$post->post_title?>" />
 	</div>
 	<?php } ?>
