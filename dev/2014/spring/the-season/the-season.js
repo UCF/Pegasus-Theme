4a3e5f6a7909dfe324b0fb471a4d38b331da3033
Jo Greybill <jogreybill@gmail.com>
2014-02-26 15:35:15 -0500

    The Season markup and js functionality

--- dev/2014/spring/the-season/the-season.js
+++ dev/2014/spring/the-season/the-season.js
@@ -43,4 +43,91 @@
 })( jQuery );
 
 
-$('.story h1').fitText(0.8, { minFontSize: '33px', maxFontSize: '215px' });
+
+// Resizing title
+$('.story h1').fitText(0.8, { minFontSize: '33px' });
+
+
+// Header img height
+function storyHeadHeight() {
+    if ($(window).outerWidth() > 767) {
+        if ($(window).outerHeight() < $(window).outerWidth()) {
+            $('#story-head').height($(window).height());
+        }
+        else {
+            $('#story-head').height($(window).outerWidth());
+        }
+    }
+    else {
+        $('#story-head').css('height', 'auto');
+    }
+}
+storyHeadHeight();
+$(window).on('resize', function() {
+    storyHeadHeight();
+});
+
+
+// Super fun parallax-y things
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.story h1').css({
+        'top': (scrolled * 0.3) + 'px',
+    });
+}
+$(window).scroll(function(e){
+    parallax();
+});
+
+
+// Lazyload all the photos
+//$('.story .game img.lazy').lazyload({'threshold': 300});
+
+
+// Set a fixed height for #game-nav so affixing behaves
+var setGameNavHeight = function() {
+    $('#game-nav-placeholder').css('height', $('#game-nav').height());
+}
+setGameNavHeight();
+$(window).on('resize', function() {
+    setGameNavHeight();
+});
+
+
+// Handle nav btn clicks, anchor jumps
+var scrollToAnchor = function(elem) {
+    $('body').animate({
+        scrollTop: elem.offset().top - ($('#game-nav-placeholder').height() + 40)
+    }, 500);
+}
+$('#game-nav .logo a').on('click', function(e) {
+    e.preventDefault();
+    scrollToAnchor($($(this).attr('href')));
+});
+$(window).on('load', function(){
+    var anchor = window.location.hash.substring(1),
+        section = $('#' + anchor);
+    if (anchor !== null && section.length > 0) {
+        scrollToAnchor(section);
+    }
+});
+
+
+// Handle affixing/active nav btns
+$(window).bind('scroll', function() {
+    // Affix nav if viewport is somewhere between the bottom of #story-head and the top of #team-stats
+    if ($(window).scrollTop() > $('#story-head').height() && $(window).scrollTop() < $('#team-stats').offset().top) {
+        $('#game-nav').addClass('affixed');
+    }
+    else {
+        $('#game-nav').removeClass('affixed');
+    }
+
+    $('.game').each(function() {
+        var game = $(this);
+        if ($(window).scrollTop() >= (game.offset().top - $('#game-nav-placeholder').height() - 40)) { // -40 to accomodate for extra pad on .logo>a click
+            $('#game-nav .logo.active').removeClass('active');
+            $('#game-nav .logo-' + game.attr('id')).addClass('active');
+        }
+    });
+});
