904fd3cb1d197a923091baeb8989b8f474d7aa79
RJ Bruneel <rj.bruneel@ucf.edu>
2015-02-20 17:24:02 -0500

    Using bootstrap tool tips

--- dev/2015/spring/nano-wire/nano-wire.js
+++ dev/2015/spring/nano-wire/nano-wire.js
@@ -1,82 +1,4 @@
 
-// Custom built fade in tool tip plugin
-
-(function($) {
-
-  $.fadeInToolTip = function(element) {
-
-    var plugin = this,
-        $triggerElement = $(element),
-        $toolTipElement = $('.' + $triggerElement.attr('data-tool-tip-content')),
-        $toolTipClass = 'fade-in-tool-tip';
-
-    function tootlTipCloseAnime($element) {
-      $element.animate({
-        top: "-=15",
-        opacity: 0
-      }, 500, function() {
-        $element.hide();
-      });
-    }
-
-    function toggleToolTip(e) {
-      e.preventDefault();
-
-      if($toolTipElement.is(':visible')) {
-        tootlTipCloseAnime($toolTipElement);
-      } else {
-        tootlTipCloseAnime($('.' + $toolTipClass + ':visible'));
-        $toolTipElement.show().animate({
-          top: "+=15",
-          opacity: 1.0
-        });
-      }
-    }
-
-    function closeToolTip() {
-      $triggerElement.trigger('click');
-    }
-
-    function setupEventHandlers() {
-      $triggerElement.on('click', toggleToolTip);
-      $toolTipElement.on('click', '.close', closeToolTip);
-    }
-
-    function positionToolTip() {
-      // nano-wire-img class shouldn't be hard coded
-      var toolTipOffset = $('.nano-wire-img').offset().top;
-
-      if($( window ).width() > 980) {
-        toolTipOffset = toolTipOffset - 100;
-      } else if($( window ).width() > 480) {
-        toolTipOffset = toolTipOffset - 250;
-      }
-      $toolTipElement.offset({ top: toolTipOffset - 75});
-    }
-
-    plugin.init = function() {
-      setupEventHandlers();
-      positionToolTip();
-    };
-
-    plugin.init();
-
-  };
-
-  $.fn.fadeInToolTip = function() {
-
-    return this.each(function() {
-      if (undefined === $(this).data('fadeInToolTip')) {
-        var plugin = new $.fadeInToolTip(this);
-
-        $(this).data('fadeInToolTip', plugin);
-      }
-    });
-
-  };
-
-})(jQuery);
-
 // Sonar Animation
 
 function animateSonar($sonar) {
@@ -105,24 +27,14 @@ function setupWayPoint($fadeInToolTipTrigger) {
   });
 }
 
-// This should be part of the plugin, but running out of time
-function setupClickToClose() {
-  $(document).on('click', function(event) {
-    if ($('.fade-in-tool-tip').is(':visible') && !$(event.target).hasClass('fade-in-tool-tip-trigger') && !$(event.target).closest('.fade-in-tool-tip').length) {
-      $('.fade-in-tool-tip:visible').hide();
-    }
-  });
-}
-
 // Init
 
 function wireArticleInit() {
 
   var $fadeInToolTipTrigger = $('.fade-in-tool-tip-trigger');
 
-  $fadeInToolTipTrigger.fadeInToolTip();
   setupWayPoint($fadeInToolTipTrigger);
-  setupClickToClose();
+  $fadeInToolTipTrigger.popover();
 
 }
 
