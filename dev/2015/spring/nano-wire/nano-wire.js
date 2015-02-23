d6f13d6b40501ee9ec10a3aeb308390dd3c31c03
RJ Bruneel <rj.bruneel@ucf.edu>
2015-02-23 15:39:56 -0500

    Minor Tweaks

--- dev/2015/spring/nano-wire/nano-wire.js
+++ dev/2015/spring/nano-wire/nano-wire.js
@@ -1,40 +1,45 @@
 
-// Sonar Animation
+(function() {
 
-function animateSonar($sonar) {
-  if($sonar && $sonar.hasClass('sonar-anime')) {
-    // hide element because the animation reverses when the sonar-anime class is removed
-    $sonar.hide();
-    setTimeout(function() { $sonar.show(); }, 2000);
+  // Sonar Animation
+
+  function animateSonar($sonar) {
+    if($sonar && $sonar.hasClass('sonar-anime')) {
+      // hide element because the animation reverses when the sonar-anime class is removed
+      $sonar.hide();
+      setTimeout(function() { $sonar.show(); }, 2000);
+    }
+    $sonar.toggleClass('sonar-anime');
   }
-  $sonar.toggleClass('sonar-anime');
-}
 
-function setupWayPoint($fadeInToolTipTrigger) {
-  // add waypoint trigger to animate in dots
-  var waypoint = new Waypoint({
-    element: $('.nano-wire-img'),
-    handler: function(direction) {
-      if(direction === "down") {
-        $('.sonar').show();
-        setInterval(animateSonar, 3000, $('.sonar'));
-        // Tool tips must have a common class otherwise multiple tool tips will be visible
-        $fadeInToolTipTrigger.addClass('show');
-        this.destroy();
-      }
-    },
-    offset: 100
-  });
-}
+  // Setup Way Point
 
-// Init
+  function setupWayPoint($fadeInToolTipTrigger, $sonar) {
+    // add waypoint trigger to animate in dots
+    var waypoint = new Waypoint({
+      element: $('.nano-wire-img'),
+      handler: function(direction) {
+        if(direction === "down") {
+          $fadeInToolTipTrigger.addClass('show');
+          $sonar.show();
+          setInterval(animateSonar, 3000, $sonar);
+          this.destroy();
+        }
+      },
+      offset: 100
+    });
+  }
 
-function wireArticleInit() {
-  var $fadeInToolTipTrigger = $('.fade-in-tool-tip-trigger');
-  $fadeInToolTipTrigger.popover();
-  $.getScript(THEME_JS_URL + '/jquery.waypoints.min.js').done(function() {
-    setupWayPoint($fadeInToolTipTrigger);
-  });
-}
+  // Init
 
-$(wireArticleInit);
+  function wireArticleInit() {
+    var $fadeInToolTipTrigger = $('.fade-in-tool-tip-trigger');
+    $fadeInToolTipTrigger.popover();
+    $.getScript(THEME_JS_URL + '/jquery.waypoints.min.js').done(function() {
+      setupWayPoint($fadeInToolTipTrigger, $('.sonar'));
+    });
+  }
+
+  $(wireArticleInit);
+
+}());
