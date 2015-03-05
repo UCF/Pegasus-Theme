9dd3b65147d44efe2f31043fff1627cffd155c96
Jo Dickson <jogreybill@gmail.com>
2015-03-05 10:49:32 -0600

    Removed unused files

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
 require_once('functions/base.php');   			# Base theme functions
-require_once('functions/feeds.php');			# Where functions related to feed data live
 require_once('custom-taxonomies.php');  		# Where per theme taxonomies are defined
 require_once('custom-post-types.php');  		# Where per theme post types are defined
 require_once('functions/admin.php');  			# Admin/login functions
