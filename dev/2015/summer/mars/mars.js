1b8408515e0d04e4fc072e059d4907fe294dbc72
RJ Bruneel <rj.bruneel@ucf.edu>
2015-07-01 14:48:46 -0400

    Mars Story Affix Logic Refined

--- dev/2015/summer/mars/mars.js
+++ dev/2015/summer/mars/mars.js
@@ -1,99 +1,97 @@
+/* global THEME_COMPONENTS_URL */
 /// <reference path="../../../../typings/jquery/jquery.d.ts"/>
 
-var $sideElevation,
-    diagramCopyHeadingArray,
-    $elevationImage,
-    robotContainerHeight,
-    diagramCopyPositionArray = [];
+(function () {
+    var $sideElevation,
+        $elevationImage,
+        diagramHeadingArray,
+        diagramContainerHeight,
+        diagramAffixBottom,
+        diagramArray = [];
 
-(function ($) {
-    $.fn.sonar = function () {
-        function animateSonar($sonar) {
-            if ($sonar && $sonar.hasClass('sonar-anime')) {
-                // hide element because the animation reverses when the sonar-anime class is removed
-                $sonar.hide();
-                setTimeout(function () { $sonar.show(); }, 2000);
+    function affixDiagram() {
+        var affixTop = $sideElevation.offset().top;
+        $sideElevation.affix({
+            offset: {
+                top: affixTop
             }
-            $sonar.toggleClass('sonar-anime');
-        }
-        return this.each(function () {
-            $(this).show();
-            setInterval(animateSonar, 3000, $(this));
         });
-    };    
-} (jQuery));
-
-function affixRobot() {
-    var affixTop = $sideElevation.offset().top;
-    $sideElevation.affix({
-        offset: {
-            top: affixTop
-        }
-    });
-} 
-
-function scrollToDesc(index) {    
-    var scrollTo = diagramCopyHeadingArray.eq(index).offset().top - (robotContainerHeight + 20);
-    $('html,body').animate({
-        scrollTop: scrollTo
-    }, 500);
-}
-
-function setImage(index) {
-    // TODO: dynamically get the image path
-    $elevationImage.attr('src', '/pegasus/wp-content/themes/Pegasus-Theme/dev/2015/summer/mars/img/elevation' + index + '.png');
-}
-
-function setupDiagramEventHandlers() {    
-    $('map[name=RobotMap] area').on('click', function () {
-        var index = $(this).index() + 1;
-        scrollToDesc(index);
-    });
-}
-
-function setupScrollEventHandlers() {
-    $(window).on('scroll', function () {
-        var scrollPosition = window.pageYOffset;  
-        if (scrollPosition >= diagramCopyPositionArray[6]) {
-            setImage(6);
-        } else if (scrollPosition >= diagramCopyPositionArray[5]) {
-            setImage(5);
-        } else if (scrollPosition >= diagramCopyPositionArray[4]) {
-            setImage(4);
-        } else if (scrollPosition >= diagramCopyPositionArray[3]) {
-            setImage(3);
-        } else if (scrollPosition >= diagramCopyPositionArray[2]) {
-            setImage(2);
-        } else if (scrollPosition >= diagramCopyPositionArray[1]) {
-            setImage(1);
-        } else if (scrollPosition < diagramCopyPositionArray[0]) {
-            setImage(0);
-        }
-    });
-}
-
-function populateDiagramCopyArray() {
-    var tempArray = [];
-    $.each(diagramCopyHeadingArray, function (i) {
-        tempArray[i] = diagramCopyHeadingArray.eq(i).offset().top - robotContainerHeight;
-    });
-    diagramCopyPositionArray = tempArray;
-}
-
-$(function () {    
-    function init() {
-        $sideElevation = $sideElevation = $('#side-elevation');
-        diagramCopyHeadingArray = $('.diagram-copy').find('h2');
-        $elevationImage = $sideElevation.find('img');
-        robotContainerHeight = $elevationImage.outerHeight();
-        populateDiagramCopyArray();
-        // $('.sonar').sonar();
-        affixRobot();
-        setupDiagramEventHandlers();
-        setupScrollEventHandlers();
-        $('map').imageMapResize();
     }
-    $(init);
-});
 
+    function scrollToDesc(index) {
+        var scrollTo = diagramHeadingArray.eq(index).offset().top - (diagramContainerHeight + 20);
+        $('html,body').animate({
+            scrollTop: scrollTo
+        }, 500);
+    }
+
+    function setImage(imgSrc) {
+        $elevationImage.attr('src', imgSrc);
+    }
+
+    function setupDiagramEventHandlers() {
+        $('map[name=RobotMap] area').on('click', function () {
+            var index = $(this).index() + 1;
+            scrollToDesc(index);
+        });
+    }
+
+    function setupScrollEventHandlers() {
+        $(window).on('scroll', function () {
+            var scrollPosition = window.pageYOffset;
+
+            if (scrollPosition > diagramAffixBottom) {
+                $sideElevation.attr('style', 'position: absolute; top: ' + diagramAffixBottom + 'px');
+            } else {
+                $sideElevation.attr('style', '');
+            }
+
+            if (scrollPosition >= diagramArray[6].position) {
+                setImage(diagramArray[6].image);
+            } else if (scrollPosition >= diagramArray[5].position) {
+                setImage(diagramArray[5].image);
+            } else if (scrollPosition >= diagramArray[4].position) {
+                setImage(diagramArray[4].image);
+            } else if (scrollPosition >= diagramArray[3].position) {
+                setImage(diagramArray[3].image);
+            } else if (scrollPosition >= diagramArray[2].position) {
+                setImage(diagramArray[2].image);
+            } else if (scrollPosition >= diagramArray[1].position) {
+                setImage(diagramArray[1].image);
+            } else if (scrollPosition < diagramArray[0].position) {
+                setImage(diagramArray[0].image);
+            }
+        });
+    }
+
+    function setDiagramCopyArray() {
+        $.each(diagramHeadingArray, function (i) {
+            var diagramObj = {};
+            diagramObj.position = diagramHeadingArray.eq(i).offset().top - diagramContainerHeight;
+            diagramObj.image = diagramHeadingArray.eq(i).attr('data-elevation-image');
+            diagramArray.push(diagramObj);
+        });
+    }
+
+    $(function () {
+        function init() {
+            $sideElevation = $sideElevation = $('#side-elevation');
+            $elevationImage = $sideElevation.find('img');
+            diagramHeadingArray = $('.diagram-copy').find('h2');
+            diagramContainerHeight = $sideElevation.outerHeight();
+            diagramAffixBottom = ($('main').outerHeight() + $('header').outerHeight()) - diagramContainerHeight;
+
+            setDiagramCopyArray();
+            affixDiagram();
+            setupDiagramEventHandlers();
+            setupScrollEventHandlers();
+        
+            // make the image map responsive
+            $.getScript(THEME_COMPONENTS_URL + '/imageMapResizer.min.js').done(function () {
+                $('map').imageMapResize();
+            });
+        }
+        $(init);
+    });
+})();
 
