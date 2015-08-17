8c5686280b1d3515cb6c759d72a7498c171a98fc
Jo Dickson <jogreybill@gmail.com>
2015-08-17 15:58:11 -0500

    background image fixes

--- dev/2012/fall/campus-wildlife/campus-wildlife.js
+++ dev/2012/fall/campus-wildlife/campus-wildlife.js
@@ -3,19 +3,19 @@ $(function() {
 	var defaultBg = $('#wildlife_body_content').attr('style');
 	var contentBgReset = function() {
 		if ($(window).width() > 767 && $(window).width() < 979) {
-			$('#wildlife_body_content').attr('style', 'background: url("'+ $('#wildlife_body_content').attr('data-bg-small') + '") top center no-repeat;');
+			$('#wildlife_body_content').attr('style', 'background-image: url("'+ $('#wildlife_body_content').attr('data-bg-small') + '"); background-position: top center; background-repeat: no-repeat;');
 		}
 		else {
 			$('#wildlife_body_content').attr('style', defaultBg);
 		}
 	}
-	
+
 	contentBgReset();
 	$(window).resize(function() {
 		contentBgReset();
 	});
-	
-	
+
+
 	// Initiate Modal window with custom styles for larger window
 	$('#habitat-modal').on('show', function() {
 		$(this).css({
