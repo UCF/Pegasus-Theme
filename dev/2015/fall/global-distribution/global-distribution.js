95660b8969e5407c7a718f8c807804870435acf4
Jim Barnes <james.barnes@ucf.edu>
2015-10-15 16:24:23 -0400

    Added images to global distribution story.

--- dev/2015/fall/global-distribution/global-distribution.js
+++ dev/2015/fall/global-distribution/global-distribution.js
@@ -383,3 +383,22 @@ if (typeof jQuery !== 'undefined') {
 } else {
 	console.log("No jQuery!");
 }
+
+// .modal-backdrop classes
+$(".modal-transparent").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  }, 0);
+});
+$(".modal-transparent").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+});
+
+$(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
