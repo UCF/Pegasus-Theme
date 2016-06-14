b6b977091fa118497e53a9167b1e947950f6059e
RJ Bruneel <rj.bruneel@ucf.edu>
2016-06-14 16:17:23 -0400

    Why I Jumped Story Intial commit

---
+++ dev/2016/summer/why-i-jumped/why-i-jumped.js
@@ -0,0 +1,46 @@
+$(function() {
+        // Returns a comma-separated numerical string.
+    // http://stackoverflow.com/a/16228123
+    function commaSeparateNumber(val) {
+        while (/(\d+)(\d{3})/.test(val.toString())) {
+            val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        }
+        return val;
+    }
+
+    // Animates increment of a number (no decimals). Handles commas.
+    function animateNumber($num) {
+        var numText = $num.text(),
+            numTextParsed = parseInt($num.text().replace(/\D/g, ''), 10);
+
+        $num.css({
+            "visibility": "visible",
+            "width": $num.width() // force fixed width to reduce flicker
+        });
+
+        $({number: 0}).animate({number: numTextParsed}, {
+            duration: 5 * 60 * 1000,
+            easing: 'linear',
+            step: function() {
+                $num.text(commaSeparateNumber(numTextParsed - Math.floor(this.number)) + ' ft');
+            },
+            done: function() {
+                $num
+                .text('') // sometimes the animation doesn't animate the last incremention for whatever reason, so force it
+                .css('width', ''); // removed forced width
+            }
+        });
+    }
+
+    function startNumberAnime() {
+        $('.counter').each(function (index, element) {
+            var $element = $(element);
+            setTimeout(function () {
+                animateNumber($element);
+            }, 1000);
+        });
+    }
+
+    startNumberAnime();
+
+});
