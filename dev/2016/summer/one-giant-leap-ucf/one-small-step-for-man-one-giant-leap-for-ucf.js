42f59f8dd1dc9bd476def9df0aaa5c41cddd2f04
Keegan Berry <keeganberry@gmail.com>
2016-06-15 16:47:41 -0400

    continued development on space timeline

---
+++ dev/2016/summer/one-giant-leap-ucf/one-small-step-for-man-one-giant-leap-for-ucf.js
@@ -0,0 +1,15 @@
+// $('body').addClass('withbg');
+
+// $(window).scroll(function() {
+//     var x = $(this).scrollTop();
+//     var start = -200;
+//     $('body').css('background-position', 'center bottom ' + parseInt(x / 50 + start) + 'px');
+// });
+
+// $(window).scroll(function() {
+//     var scroll = $(this).scrollTop() + $('#footer-navigation').outerHeight() +  $(window).outerHeight();
+//     var height = $('#footer-navigation').offset().top + $('#footer-navigation').outerHeight() + $(window).outerHeight();
+//     var start = -50;
+
+//     console.log(scroll, height, scroll/height);
+// });
