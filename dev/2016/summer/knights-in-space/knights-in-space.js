324bedeb058d50be07ff5f632a6d96335c93b197
Jo Dickson <jogreybill@gmail.com>
2016-06-23 11:33:45 -0500

    Fixed positioning of modal toggles on window resize by replacing with absolutely-positioned links; improved readability of non-bold text

--- dev/2016/summer/knights-in-space/knights-in-space.js
+++ dev/2016/summer/knights-in-space/knights-in-space.js
@@ -14,9 +14,7 @@ var KnightsInSpace;
             if ($(window).width() > 767) {
                 $intro.modal('show');
             }
-            $(window).resize(this.scaleImageMap);
             $('.expand-toggle').click(this.expand);
-            setTimeout(this.scaleImageMap, 100); // Wait 100ms for scale to make sure image is loaded.
         }
         App.prototype.addDetail = function (e) {
             var target = $(e.target).find('.modal-body');
@@ -25,36 +23,6 @@ var KnightsInSpace;
                 $(markup).prepend('<button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">&times;</span></button>');
                 target.append(markup);
             }
-        };
-        App.prototype.scaleImageMap = function () {
-            // Make sure background width and height are set.
-            this.backgroundWidth = this.backgroundWidth ? this.backgroundWidth : 2000;
-            this.backgroundHeight = this.backgroundHeight ? this.backgroundHeight : 1175;
-            // Cache background as we will be referring to it quite a bit.
-            var $background = $('.background');
-            // Calculate scale based on new width-height divided by old width/height.
-            var scale = {
-                x: $background.width() / this.backgroundWidth,
-                y: $background.height() / this.backgroundHeight
-            };
-            // Set old width/height to the new values.
-            this.backgroundWidth = $background.width();
-            this.backgroundHeight = $background.height();
-            var $map = $('#planetmap');
-            $map.find('area').each(function (i, a) {
-                var coords = $(a).attr('coords'), cSplit = coords.split(','), scaledCoords = new Array();
-                for (var c in cSplit) {
-                    var index = parseInt(c), origVal = parseInt(cSplit[c]), newVal = 0;
-                    if (index === 1 || index === 3) {
-                        newVal = Math.floor(origVal * scale.y);
-                    }
-                    else {
-                        newVal = Math.floor(origVal * scale.x);
-                    }
-                    scaledCoords.push(newVal);
-                }
-                $(a).attr('coords', scaledCoords.join(','));
-            });
         };
         App.prototype.expand = function (e) {
             e.preventDefault();
