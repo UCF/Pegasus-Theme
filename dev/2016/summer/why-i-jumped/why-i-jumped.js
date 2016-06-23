6742058fc59a42f3b890a8fcf93d4cad0eb9c0f3
RJ Bruneel <rj.bruneel@ucf.edu>
2016-06-23 14:29:09 -0400

    Moved the play/pause button, restart counter on completion, prevent jumper from scrolling out of view

--- dev/2016/summer/why-i-jumped/why-i-jumped.js
+++ dev/2016/summer/why-i-jumped/why-i-jumped.js
@@ -14,7 +14,6 @@ $(function() {
             numTextParsed = parseInt($num.text().replace(/\D/g, ''), 10);
 
         $num.css({
-            "visibility": "visible",
             "width": $num.width() // force fixed width to reduce flicker
         });
 
@@ -25,9 +24,8 @@ $(function() {
                 $num.text(commaSeparateNumber(numTextParsed - Math.floor(this.number)) + ' ft');
             },
             done: function() {
-                $num
-                .text('') // sometimes the animation doesn't animate the last incremention for whatever reason, so force it
-                .css('width', ''); // removed forced width
+                $num.text('135,890');
+                startNumberAnime();
             }
         });
     }
@@ -42,11 +40,17 @@ $(function() {
     }
 
     function addScrollEvent() {
-        $('.side-bar').height($('.content-container').height());
+        var $sideBar = $('.side-bar');
+        $sideBar.height($('.content-container').height());
+        var stopPoint = $sideBar.height() - $jumper.height() - 20;
 
         $(window).scroll(function (event) {
             var scroll = $(window).scrollTop();
-            $jumper.css('top', scroll);
+            if (scroll > stopPoint) {
+                $jumper.css('top', stopPoint);
+            } else {
+                $jumper.css('top', scroll);
+            }
         });
     }
 
@@ -54,7 +58,8 @@ $(function() {
         $('.pause').find('span').on('click', function() {
             $(this).toggleClass('glyphicon-pause glyphicon-play');
             $jumper.find('div:first-child').toggleClass('space-man');
-            $jumperContainer.find('.cloud,.cloud-sm,.counter,.pegasus').toggle();
+            $jumperContainer.find('.cloud,.cloud-sm,.pegasus').toggle();
+            $jumperContainer.find('.counter').toggleClass('counter-hidden counter-visible');
         });
     }
 
