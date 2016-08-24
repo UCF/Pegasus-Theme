1fbb654f1cbee835441a9baa7cf2cfb617dc768b
Jo Dickson <jogreybill@gmail.com>
2016-08-24 14:58:48 -0500

    Started updating site header/footer

--- versions/v5/functions.php
+++ versions/v5/functions.php
@@ -422,4 +422,20 @@ function display_photo_essay_slideshow( $photo_essay, $slug=null, $caption_color
 	return ob_get_clean();
 }
 
+
+/**
+ * Conditionally displays a <h1> or <span> for the site's primary title
+ * depending on the current page being viewed.
+ **/
+function display_site_title() {
+	$elem = ( is_home() || is_front_page() ) ? 'h1' : 'span';
+	ob_start();
+?>
+	<<?php echo $elem; ?> class="site-title">
+		<a href="<?php echo bloginfo( 'url' ); ?>"><?php echo bloginfo( 'name' ); ?></a>
+	</<?php echo $elem; ?>>
+<?php
+	return ob_get_clean();
+}
+
 ?>
