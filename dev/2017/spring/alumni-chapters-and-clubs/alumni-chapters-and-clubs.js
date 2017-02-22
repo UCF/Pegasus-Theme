f1ef2da71123cca6b5cdd3f488798dd88576db1d
Jim Barnes <james.barnes@ucf.edu>
2017-02-22 09:59:24 -0500

    Initial commit

---
+++ dev/2017/spring/alumni-chapters-and-clubs/alumni-chapters-and-clubs.js
@@ -0,0 +1,111 @@
+var map,
+    mapData,
+    saControl,
+    saPosition,
+    $chapterText,
+    $clubText,
+    $memberText,
+    chapterCount,
+    clubCount,
+    memberCount;
+
+var init = function() {
+  $chapterText = $('#chapters');
+  $clubText = $('#clubs');
+  $memberText = $('#members');
+
+  chapterCount = clubCount = memberCount = 0;
+
+  if(window.google && google.maps) {
+    getData();
+  } else {
+    lazyLoadGoogleMap();
+  }
+};
+
+var lazyLoadGoogleMap = function() {
+  $.getScript('//maps.google.com/maps/api/js?sensor=false&callback=getData&key=AIzaSyBQtVEuBQkAjfKe1HbdO-In1LgIuu1UEXk')
+    .fail(function(jqxhr, settings, ex) {
+      console.log(ex);
+    });
+};
+
+var getData = function() {
+  var dataUrl = $('#alumni-map').data('map');
+  $.getJSON(dataUrl, function(data) {
+    mapData = data;
+    initializeMap();
+  });
+};
+
+var initializeMap = function() {
+  var latlng = new google.maps.LatLng(39.905227, -95.419666);
+  saPosition = new google.maps.LatLng(23.133774, 45.101088);
+  var mapOptions = {
+    center: latlng,
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    scrollwheel: false
+  };
+  map = new google.maps.Map(document.getElementById('alumni-map'), mapOptions);
+
+  createControls();
+
+  for(var i in mapData) {
+    var d = mapData[i];
+    if (d.chapter) {
+      chapterCount += 1;
+    } else {
+      clubCount += 1;
+    }
+    memberCount += d.count;
+
+    setTimeout(addMarker, 100 * i, d);
+    setTimeout(updateLabels, 100 * i, chapterCount, clubCount, memberCount);
+  }
+};
+
+var createControls = function() {
+  var $sa_control = $('#sa-control').detach()[0];
+  $sa_control.addEventListener('click', function() {
+    map.panTo(saPosition);
+  });
+  map.controls[google.maps.ControlPosition.RIGHT_TOP].push($sa_control);
+
+  var $countControl = $('#alumni-info').detach()[0];
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push($countControl);
+};
+
+var addMarker = function(markerData) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(markerData.lat, markerData.lng),
+    title: markerData.name,
+    animation: google.maps.Animation.DROP
+  });
+
+  marker.addListener('click', function() {
+    var infoWindow = createInfoWindow(markerData.name, markerData.count);
+    infoWindow.open(map, marker);
+  });
+  
+  marker.setMap(map);
+};
+
+var createInfoWindow = function(name, memberCount) {
+  var content = '<div class="infoWindow"><h2>' + name + '</h2><p>Members: <span class="member-count">' + memberCount + '</span></p></div>';
+  var infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+
+  return infoWindow;
+};
+
+var updateLabels = function(x, y, z) { 
+  $chapterText.text(x);
+  $clubText.text(y);
+  $memberText.text(z);
+};
+
+jQuery(document).ready(function() {
+  init();
+});
