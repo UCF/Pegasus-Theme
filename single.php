300e21e94bd77897cc5c0cb4539f1210e709dcbf
cadie <cadie@ucf.edu>
2022-01-10 11:00:46 -0500

    Update single.php

--- single.php
+++ single.php
@@ -1,19 +1,20 @@
 <?php disallow_direct_load( 'single.php' ); ?>
 <?php get_version_header(); the_post(); ?>
 
-	<div class="row page-content" id="<?php echo $post->post_name; ?>">
+<div class="container page-content" id="<?php echo $post->post_name; ?>">
+	<div class="row my-4">
 		<div class="col-md-9">
 			<article>
-				<?php if( !is_front_page() ) { ?>
-						<h1><?php the_title(); ?></h1>
-				<?php } ?>
+				<?php if ( ! is_front_page() ) : ?>
+					<h1><?php the_title(); ?></h1>
+				<?php endif; ?>
 				<?php the_content();?>
 			</article>
 		</div>
-
-		<div id="sidebar" class="col-md-3">
+		<div id="sidebar" class="col-lg-3">
 			<?php echo get_sidebar(); ?>
 		</div>
 	</div>
+</div>
 
-<?php get_version_footer();?>
+<?php get_version_footer(); ?>
