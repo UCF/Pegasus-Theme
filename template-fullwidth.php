0362e5f43eb5be401d6bc4efbab26bf4b1075a26
cadie <cadie@ucf.edu>
2022-01-25 09:49:16 -0500

    Adjust page template and create fullwidth page template

--- template-fullwidth.php
+++ template-fullwidth.php
@@ -1,8 +1,12 @@
+<?php
+/**
+ * Template Name: Full Width
+ * Template Post Type: page, post
+ */
+?>
 <?php disallow_direct_load('page.php');?>
 <?php get_version_header(); the_post();?>
 <article id="<?php echo $post->post_name; ?>">
-	<div class="container my-4 my-lg-5">
-		<?php the_content(); ?>
-	</div>
+	<?php the_content(); ?>
 </article>
 <?php get_version_footer();?>
