927479bfe3e2b0065207e6eabb1cdaa3ff406ad3
Jim Barnes <jimbarnesdeveloper@gmail.com>
2022-01-31 16:11:01 -0500

    Added empty alt tag to images in the related-stories template. Fixed tabbing

--- versions/v6/templates/story/related-stories.php
+++ versions/v6/templates/story/related-stories.php
@@ -8,7 +8,7 @@ if ( $related_stories ) :
 	<h2>Related Stories</h2>
 	<?php foreach( $related_stories as $story ) : ?>
 		<article class="d-block mb-4">
-				<img class="img-fluid" src="<?php echo $story['thumbnail']; ?>">
+				<img alt="" class="img-fluid" src="<?php echo $story['thumbnail']; ?>">
 				<a class="h6 text-secondary stretched-link" href="<?php echo $story['url']; ?>"><?php echo $story['title']; ?></a>
 		</article>
 	<?php endforeach; ?>
