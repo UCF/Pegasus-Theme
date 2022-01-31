2434b673437584578d514793cd9c36699ba8c1d3
Jo Dickson <jogreybill@gmail.com>
2022-01-31 09:29:44 -0500

    Moved athena sc option overrides to version-specific config.php files; disabled registration of athena shortcodes prior to v6

--- versions/v3/functions/config.php
+++ versions/v3/functions/config.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 Config::$body_classes = array();
 
 
@@ -20,5 +20,30 @@ function jquery_in_header() {
     wp_enqueue_script( 'jquery' );
 }
 add_action('wp_enqueue_scripts', 'jquery_in_header');
+
+
+/**
+ * Disable various content/WYSIWYG formatting options
+ * provided by the Athena Shortcode plugin, since v5
+ * and prior roll their own WYSIWYG and formatting
+ * options.
+ *
+ * @since 6.0.0
+ * @author Jo Dickson
+ */
+add_filter( 'option_athena_sc_enable_tinymce_formatting', '__return_false' );
+add_filter( 'option_athena_sc_enable_optin_classes', '__return_false' );
+add_filter( 'option_athena_sc_enable_responsive_embeds', '__return_false' );
+add_filter( 'option_athena_sc_remove_image_dims', '__return_false' );
+
+
+/**
+ * Disable shortcodes provided by the Athena Shortcodes plugin,
+ * since this version has its own which may result in conflicts.
+ *
+ * @since 6.0.0
+ * @author Jo Dickson
+ */
+add_filter( 'athena_sc_add_shortcode', '__return_empty_array' );
 
 ?>
