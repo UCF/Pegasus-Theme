73d761fc3a5a26a8f6bee3f4d8827e0504a16f3c
Jim Barnes <jimbarnesdeveloper@gmail.com>
2022-01-31 16:52:21 -0500

    Accessibility update

--- versions/v6/templates/story/related-stories.php
+++ versions/v6/templates/story/related-stories.php
@@ -4,8 +4,8 @@ $related_stories = related_stories_get_stories( $post->ID );
 
 if ( $related_stories ) :
 ?>
-<aside>
-	<h2>Related Stories</h2>
+<aside aria-labelledby="related-stories">
+	<h2 id="related-stories">Related Stories</h2>
 	<?php foreach( $related_stories as $story ) : ?>
 		<article class="d-block mb-5 position-relative">
 			<img alt="" class="img-fluid" src="<?php echo $story['thumbnail']; ?>">
