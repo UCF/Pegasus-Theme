765e780f4f5a3c75dd6193088865fc7c457b5f0c
Jo Dickson <jogreybill@gmail.com>
2022-02-02 11:09:59 -0500

    Removed/replaced styles in type.scss

--- versions/v6/footer.php
+++ versions/v6/footer.php
@@ -17,7 +17,7 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12 col-sm-12">
-							<h2 class="section-title">More UCF Stories</h2>
+							<h2 class="font-serif font-italic font-weight-normal my-4 py-2">More UCF Stories</h2>
 						</div>
 					</div>
 				</div>
