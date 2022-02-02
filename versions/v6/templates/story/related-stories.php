efcddb5ffb9f819fe639baed8b8da574192653f9
cadie <cadie@ucf.edu>
2022-02-02 03:31:04 -0500

    Adjust related stories markup and styles

--- versions/v6/templates/story/related-stories.php
+++ versions/v6/templates/story/related-stories.php
@@ -4,12 +4,18 @@ $related_stories = related_stories_get_stories( $post->ID );
 
 if ( $related_stories ) :
 ?>
-<aside aria-labelledby="related-stories">
-	<h2 id="related-stories">Related Stories</h2>
+<aside class="related-stories mb-4 mb-lg-5" aria-labelledby="related-stories">
+	<h2 id="related-stories" class="mb-3 mb-lg-4">Related Stories</h2>
 	<?php foreach( $related_stories as $story ) : ?>
-		<article class="d-block mb-5 position-relative">
-			<img alt="" class="img-fluid" src="<?php echo $story['thumbnail']; ?>">
-			<a class="h6 text-secondary stretched-link" href="<?php echo $story['url']; ?>"><?php echo $story['title']; ?></a>
+		<article class="row align-items-center position-relative mb-4">
+			<div class="col-sm-4 col-lg-12">
+				<img class="img-fluid w-100 d-block mb-2 mb-sm-0 mb-lg-2" src="<?php echo $story['thumbnail']; ?>" alt="" aria-hidden="true" width="300" height="200">
+			</div>
+			<div class="col-sm-8 col-lg-12 position-static">
+				<a class="stretched-link text-secondary font-weight-bold" href="<?php echo $story['url']; ?>">
+					<?php echo $story['title']; ?>
+				</a>
+			</div>
 		</article>
 	<?php endforeach; ?>
 </aside>
