ffdfc19ae5d6beaf4f69623d2842c77a90feaf5c
Jim Barnes <jimbarnesdeveloper@gmail.com>
2022-07-20 13:17:53 -0400

    Added google tag manager code to all six versions

--- versions/v6/functions/config.php
+++ versions/v6/functions/config.php
@@ -35,6 +35,22 @@ function v6_hook_frontend_theme_scripts() {
 	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 
+	<?php if ( GTM_ID ): ?>
+<script>
+	dataLayer = [];
+</script>
+<script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','<?php echo GTM_ID; ?>');</script>
+<!-- End Google Tag Manager -->
+</script>
+	<?php endif; ?>
+
+
 	<?php if( GA_ACCOUNT or CB_UID ): ?>
 
 	<script type="text/javascript">
